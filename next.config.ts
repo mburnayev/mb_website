f75f8cd1cfa693df7d655609c6db72cd413886ce
mburnayev <misha@burnayev.com>
2024-11-07 21:07:35 -0500

    added Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images : {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+        port: "",
+        pathname: "/256/**/*.png"
+      }
+    ]
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
